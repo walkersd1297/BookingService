7e075c85f73042a743fab3ddcac91ec9c3baa350
sahil dhiman <194502@nith.ac.in>
2024-10-17 19:39:06 +0530

    Add error files

---
+++ src/utils/errors/service-error.js
@@ -0,0 +1,11 @@
+const {StatusCodes} = require('http-status-codes');
+
+class ServiceError extends Error{
+    constructor(message="Something went Wrong",explanation,statusCode=StatusCodes.INTERNAL_SERVER_ERROR){
+        super();
+        this.name = "Service Error";
+        this.message = message;
+        this.explanation = explanation;
+        this.statusCode = statusCode;
+    }
+}
