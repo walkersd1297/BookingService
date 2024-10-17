4855b10ab11db9c486b484f0ec7adcdce620f100
sahil dhiman <194502@nith.ac.in>
2024-10-17 19:11:32 +0530

    initialize sequelize in the project

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-    PORT:process.env.PORT
+    PORT:process.env.PORT,
+    DB_SYNC:process.env.DB_SYNC,
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const expresss = require('express');
 const bodyParser = require('body-parser');
-const {PORT} = require('./config/serverConfig.js');
-
+const {PORT,DB_SYNC} = require('./config/serverConfig.js');
+const apiRoutes = require('./router/index.js')
+const db = require('./models/index.js');
 function setupAndStartServer(){
     const app = expresss();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api',apiRoutes);
+    if(DB_SYNC){
+        db.sequelize.sync({alter:true});
+    }
     app.listen(PORT,()=>{
         console.log(`Server started at port ${PORT}`);
     })
